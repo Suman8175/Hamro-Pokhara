7d78d47a6dec1c81224fca991626d66cc1624af2
Suman Devkota <sumandevkota75@gmail.com>
2023-12-21 15:22:08 +0545

    JSON filed added

--- lib/features/screens/splash_screen/splash_screen.dart
+++ lib/features/screens/splash_screen/splash_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yhhhamropokhara/features/models/data/color_fetch/colors_getter.dart';
 import 'package:yhhhamropokhara/features/screens/get_started/on_boarding_screen.dart';
-import 'package:yhhhamropokhara/features/screens/home_screen/bottombar.dart';
 import 'package:yhhhamropokhara/features/screens/login_signup/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
